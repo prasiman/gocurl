5e20c24f74d0ec084d8c026cb1ec3a2b148cfc4d
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2023-12-27 12:14:43 +0700

    Back to build dockerfile as executable, put them into distroless container

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM golang:latest
-
+FROM golang:latest AS build
 WORKDIR /app
 COPY . .
+RUN CGO_ENABLED=0 go build -ldflags="-w -s" -v -o /app
 
-ENTRYPOINT [ "go", "run", "/app/main.go" ]
+FROM gcr.io/distroless/static-debian11
+COPY --from=build /app /app
+ENTRYPOINT [ "/app" ]
---
+++ util/httpclient/httpclient.go
@@ -0,0 +1,84 @@
+package httpclient
+
+import (
+	"bytes"
+	"io"
+	"math"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var RetryCount = 1
+
+type retryableTransport struct {
+	transport http.RoundTripper
+}
+
+func NewRetryableClient(timeout time.Duration, retry *int, proxyUrl *string, proxyUsername *string, proxyPassword *string) *http.Client {
+	var transport = &retryableTransport{
+		transport: &http.Transport{},
+	}
+
+	if *proxyUrl != "" {
+		proxy, _ := url.Parse(*proxyUrl)
+		if *proxyUsername != "" {
+			proxy.User = url.UserPassword(*proxyUsername, *proxyPassword)
+		}
+		transport = &retryableTransport{
+			transport: &http.Transport{
+				Proxy: http.ProxyURL(proxy),
+			},
+		}
+	}
+
+	RetryCount = *retry
+
+	return &http.Client{
+		Transport: transport,
+		Timeout:   timeout,
+	}
+}
+
+func (t *retryableTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Clone the request body
+	var bodyBytes []byte
+	if req.Body != nil {
+		bodyBytes, _ = io.ReadAll(req.Body)
+		req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	}
+	// Send the request
+	resp, err := t.transport.RoundTrip(req)
+	// Retry logic
+	retries := 0
+	for shouldRetry(err, resp) && retries < RetryCount {
+		// Wait for the specified backoff period
+		time.Sleep(backoff(retries))
+		// We're going to retry, consume any response to reuse the connection.
+		drainBody(resp)
+		// Clone the request body again
+		if req.Body != nil {
+			req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
+		// Retry the request
+		resp, err = t.transport.RoundTrip(req)
+		retries++
+	}
+	// Return the response
+	return resp, err
+}
+
+func backoff(retries int) time.Duration {
+	return time.Duration(math.Pow(2, float64(retries))) * time.Second
+}
+
+func shouldRetry(err error, resp *http.Response) bool {
+	return err != nil
+}
+
+func drainBody(resp *http.Response) {
+	if resp != nil && resp.Body != nil {
+		io.Copy(io.Discard, resp.Body)
+		resp.Body.Close()
+	}
+}
